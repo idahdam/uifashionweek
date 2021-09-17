6362fa93ec1b5f93619ef2a36aafb1883525efb9
RLoo <jonathanfaren@gmail.com>
2021-09-17 12:34:05 +0700

    contact us done

--- src/components/home/contact/contact.element.js
+++ src/components/home/contact/contact.element.js
@@ -2,6 +2,13 @@ import styled from "styled-components";
 
 export const ContactContainer = styled.div`
   background-color: #ffffff;
+  background-image: radial-gradient(
+    farthest-side at 20% 100%,
+    #ffffff 0%,
+    #497099 0%,
+    #ffffff,
+    transparent 100%
+  );
   z-index: 1;
   width: 100%;
   height: 100vh;
@@ -51,12 +58,82 @@ export const ContactHeader = styled.header`
   font-weight: bold;
 `;
 
-export const AboutSection = styled.div`
+export const ContactSection = styled.div`
   margin-left: auto;
   margin-right: auto;
   width: 100%;
   overflow: hidden;
-  flex-direction: row;
-  flex-wrap: wrap;
   margin-top: 8%;
+`;
+
+export const ContactSectionLeft = styled.div`
+  width: 48%;
+  display: flex;
+  float: left;
+  margin-top: 5%;
+  height: 100%;
+`;
+export const ContactSectionRight = styled.div`
+  width: 48%;
+  display: flex;
+  float: right;
+  margin-top: 5%;
+`;
+
+export const ContactImage = styled.img`
+  margin: 0 auto;
+`;
+export const ContacSectionHeader = styled.div`
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 32px;
+  // top: 50%;
+  // left: 50%;
+  width: 70%;
+  display: flex;
+`;
+
+export const ContacSectionText = styled.div`
+Font family: Metropolis
+Font style: Regular
+Font size: 16px
+Align: Justified;
+margin-top:10%;
+// top: 50%;
+//   left: 50%;
+width: 100%;
+display: flex;
+`;
+
+export const ContactSectionColumn = styled.div`
+  position: relative;
+  margin-left: auto;
+  margin-right: auto;
+  padding-bottom: 2px;
+  margin-top: 15%;
+`;
+
+export const ContactPageButton = styled.button`
+  background: #03a301;
+  left: 0;
+  border: none;
+  border-radius: 15px;
+  position: absolute;
+  color: white;
+  width: 203px;
+  height: 47px;
+  font-family: Montserrat;
+  font-weight: bold;
+  font-size: 18px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  opacity: 1;
+  transition: 0.3s;
+  cursor: pointer;
+  margin-top: 5%;
+  &:hover {
+    opacity: 0.6;
+  }
 `;
--- src/components/home/contact/contact.js
+++ src/components/home/contact/contact.js
@@ -4,17 +4,48 @@ import {
   ContactContent,
   ContactSectionHeader,
   ContactHeader,
-  Circle
+  ContactSection,
+  ContactSectionLeft,
+  ContactSectionRight,
+  ContactImage,
+  ContacSectionHeader,
+  ContacSectionText,
+  ContactSectionColumn,
+  ContactPageButton,
 } from "./contact.element";
+import image_1 from "../../../assets/image/contact/Contact_pic.png";
+
 const Contact = () => {
   return (
     <>
       <ContactContainer>
-        <Circle />
         <ContactContent>
           <ContactSectionHeader>
             <ContactHeader>Contact</ContactHeader>
           </ContactSectionHeader>
+          <ContactSection>
+            <ContactSectionLeft>
+            <ContactSectionColumn>
+              <ContacSectionHeader>
+                Lorem ipsum dolor sit amet
+              </ContacSectionHeader>
+              <ContacSectionText>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea ommodo consequat. Duis
+                    aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                    occaecat cupidatat non proident, sunt in culpa qui officia
+                    deserunt mollit anim id est laborum"
+              </ContacSectionText>
+              <ContactPageButton> contact </ContactPageButton>
+              </ContactSectionColumn>
+            </ContactSectionLeft>
+            <ContactSectionRight>
+              <ContactImage src={image_1} />
+            </ContactSectionRight>
+          </ContactSection>
         </ContactContent>
       </ContactContainer>
     </>
