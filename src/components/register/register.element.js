6f8a6e869ee00d893df1d9279b36b0155a49f619
Hadi <nasshitell@gmail.com>
2021-09-19 19:40:08 +0700

    add register page

--- src/components/register/register.element.js
+++ src/components/register/register.element.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
-export const RegisterContainer = styled.div``;
+export const RegisterContainer = styled.div`
+  text-align: center;
+  padding: 5%;
+`;
 
 export const RegisterContainerRow = styled.div`
   display: flex;
@@ -13,3 +16,84 @@ export const RegisterContainerCol = styled.div`
 `;
 
 export const RegisterImage = styled.img``;
+
+export const RegisterCardContainer = styled.div`
+  background-color: aqua;
+  padding: 5%;
+  border-radius: 25px;
+`;
+
+export const RegisterCardTitle = styled.div`
+  font-size: 2rem;
+  padding: 2% 0;
+`;
+
+export const RegisterCardDescription = styled.div`
+  font-size: 1rem;
+`;
+
+export const RegisterFormContainer = styled.div``;
+
+export const RegisterInput = styled.input`
+  width: 100%;
+  font-family: "Metropolis Regular";
+  padding: 20px 20px;
+  margin: 8px 0;
+  font-size: 1rem;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  border-radius: 10px;
+`;
+
+export const RegisterSelect = styled.select`
+  width: 100%;
+  font-family: "Metropolis Regular";
+  padding: 20px 20px;
+  margin: 8px 0;
+  font-size: 1rem;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  border-radius: 10px;
+`;
+
+export const RegisterOption = styled.option`
+  width: 100%;
+  font-family: "Metropolis Regular";
+  padding: 20px 20px;
+  margin: 8px 0;
+  font-size: 1rem;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  border-radius: 10px;
+`;
+
+export const RegisterOptionDefault = styled.option`
+  width: 100%;
+  padding: 20px 20px;
+  margin: 8px 0;
+  opacity: 0.5;
+  -moz-opacity: 0.5;
+  -webkit-opacity: 0.5;
+  color: #999999;
+  font-size: 0.875rem;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  border-radius: 10px;
+`;
+
+export const RegisterButtonContainer = styled.div``;
+export const RegisterButton = styled.button`
+  padding: 0 1.5%;
+  margin: 0 1.5%;
+`;
+export const RegisterPageCountContainer = styled.div`
+  padding: 2.5%;
+`;
--- src/components/register/register.js
+++ src/components/register/register.js
@@ -1,13 +1,182 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   RegisterContainer,
   RegisterContainerRow,
   RegisterContainerCol,
   RegisterImage,
+  RegisterCardContainer,
+  RegisterCardTitle,
+  RegisterCardDescription,
+  RegisterFormContainer,
+  RegisterInput,
+  RegisterSelect,
+  RegisterOptionDefault,
+  RegisterOption,
+  RegisterButtonContainer,
+  RegisterButton,
+  RegisterPageCountContainer,
 } from "./register.element";
 import image from "../../assets/image/register/left.png";
 
 const Register = () => {
+  const [page, setPage] = useState(1);
+  let card;
+
+  if (page === 0) {
+    card = (
+      <>
+        <RegisterCardTitle>FAQ</RegisterCardTitle>
+        <RegisterCardDescription>Personal Data</RegisterCardDescription>
+        <RegisterFormContainer>
+          <RegisterInput placeholder="Full Name" type="text" />
+          <RegisterInput placeholder="Nickname" type="text" />
+          <RegisterSelect>
+            <RegisterOptionDefault value="" disabled selected hidden>
+              Gender
+            </RegisterOptionDefault>
+            <RegisterOption value="0">Male</RegisterOption>
+            <RegisterOption value="1">Female</RegisterOption>
+            <RegisterOption value="2">Rather Not To Say</RegisterOption>
+          </RegisterSelect>
+          <RegisterInput placeholder="Birthday..." type="text" />
+          <RegisterInput placeholder="ID Number" type="text" />
+          <RegisterInput placeholder="Profession" type="text" />
+          <RegisterInput placeholder="Address" type="text" />
+          <RegisterInput placeholder="Contact WA / LINE" type="text" />
+          <RegisterInput placeholder="Instagram" type="text" />
+        </RegisterFormContainer>
+        <RegisterPageCountContainer>(4/4)</RegisterPageCountContainer>
+        <RegisterButtonContainer>
+          <RegisterButton onClick={() => setPage(1)}>Go Back</RegisterButton>
+          {/* <RegisterButton onClick={() => console.log("last page")}>
+            Submit
+          </RegisterButton> */}
+        </RegisterButtonContainer>
+      </>
+    );
+  } else if (page === 1) {
+    card = (
+      <>
+        <RegisterCardTitle>Model Hunt Registration</RegisterCardTitle>
+        <RegisterCardDescription>Personal Data</RegisterCardDescription>
+        <RegisterFormContainer>
+          <RegisterInput placeholder="Full Name" type="text" />
+          <RegisterInput placeholder="Nickname" type="text" />
+          <RegisterSelect>
+            <RegisterOptionDefault value="" disabled selected hidden>
+              Gender
+            </RegisterOptionDefault>
+            <RegisterOption value="0">Male</RegisterOption>
+            <RegisterOption value="1">Female</RegisterOption>
+            <RegisterOption value="2">Rather Not To Say</RegisterOption>
+          </RegisterSelect>
+          <RegisterInput placeholder="Birthday..." type="text" />
+          <RegisterInput placeholder="ID Number" type="text" />
+          <RegisterInput placeholder="Profession" type="text" />
+          <RegisterInput placeholder="Address" type="text" />
+          <RegisterInput placeholder="Contact WA / LINE" type="text" />
+          <RegisterInput placeholder="Instagram" type="text" />
+        </RegisterFormContainer>
+        <RegisterPageCountContainer>(1/4)</RegisterPageCountContainer>
+        <RegisterButtonContainer>
+          <RegisterButton onClick={() => setPage(0)}>FAQ</RegisterButton>
+          <RegisterButton onClick={() => setPage(2)}>Next</RegisterButton>
+        </RegisterButtonContainer>
+      </>
+    );
+  } else if (page === 2) {
+    card = (
+      <>
+        <RegisterCardTitle>Model Hunt Registration</RegisterCardTitle>
+        <RegisterCardDescription>Personal Data</RegisterCardDescription>
+        <RegisterFormContainer>
+          <RegisterInput placeholder="Full Name" type="text" />
+          <RegisterInput placeholder="Nickname" type="text" />
+          <RegisterSelect>
+            <RegisterOptionDefault value="" disabled selected hidden>
+              Gender
+            </RegisterOptionDefault>
+            <RegisterOption value="0">Male</RegisterOption>
+            <RegisterOption value="1">Female</RegisterOption>
+            <RegisterOption value="2">Rather Not To Say</RegisterOption>
+          </RegisterSelect>
+          <RegisterInput placeholder="Birthday..." type="text" />
+          <RegisterInput placeholder="ID Number" type="text" />
+          <RegisterInput placeholder="Profession" type="text" />
+          <RegisterInput placeholder="Address" type="text" />
+          <RegisterInput placeholder="Contact WA / LINE" type="text" />
+          <RegisterInput placeholder="Instagram" type="text" />
+        </RegisterFormContainer>
+        <RegisterPageCountContainer>(2/4)</RegisterPageCountContainer>
+        <RegisterButtonContainer>
+          <RegisterButton onClick={() => setPage(1)}>Back</RegisterButton>
+          <RegisterButton onClick={() => setPage(3)}>Next</RegisterButton>
+        </RegisterButtonContainer>
+      </>
+    );
+  } else if (page === 3) {
+    card = (
+      <>
+        <RegisterCardTitle>Model Hunt Registration</RegisterCardTitle>
+        <RegisterCardDescription>Personal Data</RegisterCardDescription>
+        <RegisterFormContainer>
+          <RegisterInput placeholder="Full Name" type="text" />
+          <RegisterInput placeholder="Nickname" type="text" />
+          <RegisterSelect>
+            <RegisterOptionDefault value="" disabled selected hidden>
+              Gender
+            </RegisterOptionDefault>
+            <RegisterOption value="0">Male</RegisterOption>
+            <RegisterOption value="1">Female</RegisterOption>
+            <RegisterOption value="2">Rather Not To Say</RegisterOption>
+          </RegisterSelect>
+          <RegisterInput placeholder="Birthday..." type="text" />
+          <RegisterInput placeholder="ID Number" type="text" />
+          <RegisterInput placeholder="Profession" type="text" />
+          <RegisterInput placeholder="Address" type="text" />
+          <RegisterInput placeholder="Contact WA / LINE" type="text" />
+          <RegisterInput placeholder="Instagram" type="text" />
+        </RegisterFormContainer>
+        <RegisterPageCountContainer>(3/4)</RegisterPageCountContainer>
+        <RegisterButtonContainer>
+          <RegisterButton onClick={() => setPage(2)}>Back</RegisterButton>
+          <RegisterButton onClick={() => setPage(4)}>Next</RegisterButton>
+        </RegisterButtonContainer>
+      </>
+    );
+  } else if (page === 4) {
+    card = (
+      <>
+        <RegisterCardTitle>Model Hunt Registration</RegisterCardTitle>
+        <RegisterCardDescription>Personal Data</RegisterCardDescription>
+        <RegisterFormContainer>
+          <RegisterInput placeholder="Full Name" type="text" />
+          <RegisterInput placeholder="Nickname" type="text" />
+          <RegisterSelect>
+            <RegisterOptionDefault value="" disabled selected hidden>
+              Gender
+            </RegisterOptionDefault>
+            <RegisterOption value="0">Male</RegisterOption>
+            <RegisterOption value="1">Female</RegisterOption>
+            <RegisterOption value="2">Rather Not To Say</RegisterOption>
+          </RegisterSelect>
+          <RegisterInput placeholder="Birthday..." type="text" />
+          <RegisterInput placeholder="ID Number" type="text" />
+          <RegisterInput placeholder="Profession" type="text" />
+          <RegisterInput placeholder="Address" type="text" />
+          <RegisterInput placeholder="Contact WA / LINE" type="text" />
+          <RegisterInput placeholder="Instagram" type="text" />
+        </RegisterFormContainer>
+        <RegisterPageCountContainer>(4/4)</RegisterPageCountContainer>
+        <RegisterButtonContainer>
+          <RegisterButton onClick={() => setPage(3)}>Back</RegisterButton>
+          <RegisterButton onClick={() => console.log("last page")}>
+            Submit
+          </RegisterButton>
+        </RegisterButtonContainer>
+      </>
+    );
+  }
   return (
     <>
       <RegisterContainer>
@@ -16,7 +185,7 @@ const Register = () => {
             <RegisterImage src={image} alt="image" />
           </RegisterContainerCol>
           <RegisterContainerCol>
-            <RegisterImage src={image} alt="image" />
+            <RegisterCardContainer>{card}</RegisterCardContainer>
           </RegisterContainerCol>
         </RegisterContainerRow>
       </RegisterContainer>
