7c94d06539d285eb38756c7243caefe6f70b039a
RLoo <jonathanfaren@gmail.com>
2021-09-20 10:41:14 +0700

    update background and font

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,24 +1,43 @@
 import React from "react";
 import {
-Footer,
-FooterContainer,
-FooterLeft,
-FooterRight,
-FooterLink,
+  Footer,
+  FooterContainer,
+  FooterLeft,
+  FooterLogoContainer,
+  FooterLink,
+  FooterText,
 } from "./footer.elements";
 
+import Linkedin_logo from "../../assets/image/footer/Group 111.png";
+import Insta_logo from "../../assets/image/footer/Group 112.png";
+import Youtube_logo from "../../assets/image/footer/Group 113.png";
+
 const footer = () => {
-return (
-	<Footer>
-	<FooterContainer>
-		<FooterLeft>
-		Copyright @UI Fashion Week
-		</FooterLeft>
-		<FooterLeft>
-			buat logo 
-			</FooterLeft>
-	</FooterContainer>
-	</Footer>
-);
+  return (
+    <Footer>
+      <FooterContainer>
+        <FooterLeft>
+          <FooterText>Copyright @UI Fashion Week</FooterText>
+        </FooterLeft>
+        <FooterLeft>
+          <FooterLogoContainer>
+            <FooterLink href="#">
+              <img src={Linkedin_logo} />{" "}
+            </FooterLink>
+          </FooterLogoContainer>
+          <FooterLogoContainer>
+            <FooterLink href="#">
+              <img src={Insta_logo} />
+            </FooterLink>
+          </FooterLogoContainer>
+          <FooterLogoContainer>
+            <FooterLink href="#">
+              <img src={Youtube_logo} />
+            </FooterLink>
+          </FooterLogoContainer>
+        </FooterLeft>
+      </FooterContainer>
+    </Footer>
+  );
 };
 export default footer;
